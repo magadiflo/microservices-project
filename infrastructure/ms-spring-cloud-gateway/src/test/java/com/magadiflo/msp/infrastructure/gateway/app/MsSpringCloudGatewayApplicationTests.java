cdb676f7927980c44d994c08fbb06f700073a0f3
Martín <magadiflo@gmail.com>
2023-05-01 12:21:35 -0500

    Integrando Spring Cloud Gateway (alternativa actual a Zuul)

---
+++ infrastructure/ms-spring-cloud-gateway/src/test/java/com/magadiflo/msp/infrastructure/gateway/app/MsSpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.msp.infrastructure.gateway.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
