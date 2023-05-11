908c601ce61de7e2497b9dc85ae6a4934412b8b8
Martín <magadiflo@gmail.com>
2023-05-11 17:44:46 -0500

    Agregando nuevo módulo ms-usuarios

---
+++ security/ms-usuarios/src/test/java/com/magadiflo/msp/security/usuarios/app/MsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.msp.security.usuarios.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
