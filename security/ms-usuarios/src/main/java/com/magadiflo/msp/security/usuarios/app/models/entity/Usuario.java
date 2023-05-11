612ea830ab8cf543f3199e91e4a5017f9c523429
Martín <magadiflo@gmail.com>
2023-05-11 18:25:39 -0500

    Creando las entidades necesarias Usuario y Rol

---
+++ security/ms-usuarios/src/main/java/com/magadiflo/msp/security/usuarios/app/models/entity/Usuario.java
@@ -0,0 +1,94 @@
+package com.magadiflo.msp.security.usuarios.app.models.entity;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, length = 20)
+    private String username;
+    @Column(length = 60)
+    private String password;
+    private Boolean enabled;
+    private String nombre;
+    private String apellido;
+    @Column(unique = true, length = 100)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Usuario{");
+        sb.append("id=").append(id);
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", enabled=").append(enabled);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", apellido='").append(apellido).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
