b828cc7be4ee8aa1d78de752747d590178514e7a
Martín <magadiflo@gmail.com>
2023-05-12 17:45:37 -0500

    Agregando las mismas Entities del ms-usuarios

---
+++ shared-library/ms-usuarios-commons/src/main/java/com/magadiflo/msp/shared/library/usuarios/commons/app/models/entity/Rol.java
@@ -0,0 +1,42 @@
+package com.magadiflo.msp.shared.library.usuarios.commons.app.models.entity;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+public class Rol implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, length = 30)
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Rol{");
+        sb.append("id=").append(id);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
