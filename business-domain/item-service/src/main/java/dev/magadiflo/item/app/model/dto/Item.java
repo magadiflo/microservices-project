0903a9bbc873f89249ee1b44c92e9c5f10e946e1
Martín Díaz <magadiflo@gmail.com>
2025-04-07 15:47:10 -0500

    Test the API Items (client)

--- business-domain/item-service/src/main/java/dev/magadiflo/item/app/model/dto/Item.java
+++ business-domain/item-service/src/main/java/dev/magadiflo/item/app/model/dto/Item.java
@@ -1,8 +1,11 @@
 package dev.magadiflo.item.app.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public record Item(Product product, int quantity) {
+    @JsonProperty
     public BigDecimal total() {
         return product.price().multiply(BigDecimal.valueOf(quantity));
     }
