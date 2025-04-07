8f4785caafdb31712aae343d57583025eec88570
Martín Díaz <magadiflo@gmail.com>
2025-04-07 15:15:07 -0500

    Create ItemController

---
+++ business-domain/item-service/src/main/java/dev/magadiflo/item/app/controller/ItemController.java
@@ -0,0 +1,33 @@
+package dev.magadiflo.item.app.controller;
+
+import dev.magadiflo.item.app.model.dto.Item;
+import dev.magadiflo.item.app.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "/api/v1/items")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping
+    public ResponseEntity<List<Item>> findProducts() {
+        return ResponseEntity.ok(this.itemService.findItems());
+    }
+
+    @GetMapping(path = "/{productId}")
+    public ResponseEntity<Item> findProduct(@PathVariable Long productId, @RequestParam int quantity) {
+        return ResponseEntity.ok(this.itemService.findItemByProductId(productId, quantity));
+    }
+}
