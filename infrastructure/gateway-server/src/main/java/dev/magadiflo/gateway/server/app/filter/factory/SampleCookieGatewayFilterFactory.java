69b009be10a0b5b4f7f01358e65712a4d339c089
Martín Díaz <magadiflo@gmail.com>
2025-04-09 17:40:10 -0500

    Gateway Filter Factory Modificando Response HTTP

--- infrastructure/gateway-server/src/main/java/dev/magadiflo/gateway/server/app/filter/factory/SampleCookieGatewayFilterFactory.java
+++ infrastructure/gateway-server/src/main/java/dev/magadiflo/gateway/server/app/filter/factory/SampleCookieGatewayFilterFactory.java
@@ -4,8 +4,11 @@ import dev.magadiflo.gateway.server.app.dto.ConfigurationCookie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -18,6 +21,10 @@ public class SampleCookieGatewayFilterFactory extends AbstractGatewayFilterFacto
             return chain.filter(exchange)
                     .then(Mono.fromRunnable(() -> {
                         log.info("Ejecuta POST GatewayFilterFactory: {}", config.message());
+
+                        Optional.ofNullable(config.cookieValue()).ifPresent(cookie ->
+                                exchange.getResponse()
+                                        .addCookie(ResponseCookie.from(config.cookieName(), cookie).build()));
                     }));
         };
     }
