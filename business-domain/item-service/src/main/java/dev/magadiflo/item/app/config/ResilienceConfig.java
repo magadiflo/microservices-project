a55c8aeef2070cc0a45e273ae53f51ddbba2005c
Martín Díaz <magadiflo@gmail.com>
2025-04-10 18:11:28 -0500

    Personalizando parámetros del Circuit Breaker

---
+++ business-domain/item-service/src/main/java/dev/magadiflo/item/app/config/ResilienceConfig.java
@@ -0,0 +1,32 @@
+package dev.magadiflo.item.app.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Slf4j
+@Configuration
+public class ResilienceConfig {
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> customizerCircuitBreaker() {
+        return factory -> factory.configureDefault(circuitBreakerId -> {
+            log.info("Configurando circuit breaker: {}", circuitBreakerId);
+
+            return new Resilience4JConfigBuilder(circuitBreakerId)
+                    .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                            .slidingWindowSize(10)
+                            .failureRateThreshold(50)
+                            .waitDurationInOpenState(Duration.ofSeconds(10L))
+                            .permittedNumberOfCallsInHalfOpenState(5)
+                            .build())
+                    .build();
+        });
+    }
+}
