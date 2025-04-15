f3c87d20f134d44051e99c97e7d6ee7b4679ee9e
Martín Díaz <magadiflo@gmail.com>
2025-04-15 15:35:06 -0500

    Creando entity User y repositorio JPA

---
+++ security/user-service/src/main/java/dev/magadiflo/user/app/entity/User.java
@@ -0,0 +1,30 @@
+package dev.magadiflo.user.app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private boolean enabled;
+    @Column(unique = true)
+    private String email;
+}
