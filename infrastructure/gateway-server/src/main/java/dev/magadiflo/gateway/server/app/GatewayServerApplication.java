dfe7fe1d5448eb942b56dc9082bb90bdd88f192b
Martín Díaz <magadiflo@gmail.com>
2025-04-22 16:21:26 -0500

    Propagación automática del contexto reactivo para trazabilidad (traceId, spanId)

--- infrastructure/gateway-server/src/main/java/dev/magadiflo/gateway/server/app/GatewayServerApplication.java
+++ infrastructure/gateway-server/src/main/java/dev/magadiflo/gateway/server/app/GatewayServerApplication.java
@@ -2,11 +2,13 @@ package dev.magadiflo.gateway.server.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(GatewayServerApplication.class, args);
     }
 }
