b390704b4817ad356ff3617e1edfabc76b1b064c
Martín Díaz <magadiflo@gmail.com>
2025-04-23 18:35:48 -0500

    Propagando contexto traces entre OAuth y Users con RestClient + LoadBalanced

--- security/authorization-server/src/main/java/dev/magadiflo/authorization/server/app/config/AuthConfig.java
+++ security/authorization-server/src/main/java/dev/magadiflo/authorization/server/app/config/AuthConfig.java
@@ -1,12 +1,16 @@
 package dev.magadiflo.authorization.server.app.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 @Configuration
 public class AuthConfig {
@@ -15,9 +19,16 @@ public class AuthConfig {
     private String userServiceBaseUrl;
 
     @LoadBalanced
+    @Bean
+    public RestClient.Builder restClientBuilder(List<RestClientCustomizer> customizers) {
+        RestClient.Builder builder = RestClient.builder();
+        customizers.forEach(customizer -> customizer.customize(builder));
+        return builder;
+    }
+
     @Bean(name = "userRestClient")
-    public RestClient.Builder restClientBuilder() {
-        return RestClient.builder().baseUrl(this.userServiceBaseUrl);
+    public RestClient productRestClient(@Qualifier("restClientBuilder") RestClient.Builder builder) {
+        return builder.baseUrl(this.userServiceBaseUrl).build();
     }
 
     @Bean
